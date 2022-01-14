2403d2010e82284ccca760cf2ce158680f52d303
Y.C.Chen <Y.C.chen@icnexus.com.tw>
2022-01-14 09:28:17 +0800

    add 01 qt Button demo

---
+++ 01_qtButton/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    button = new QPushButton(this);
+    button->setGeometry(QRect(100,100,100,25));
+    connect(button, SIGNAL(released()), this, SLOT(txtButton()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::txtButton()
+{
+    button->setText("Button B");
+}
+
