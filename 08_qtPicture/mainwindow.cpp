684e19e1e61cd7f581a901438a05c466ea303488
gavin <gavinchen523@gmail.com>
2022-01-20 23:07:57 +0800

    paint line

--- 08_qtPicture/mainwindow.cpp
+++ 08_qtPicture/mainwindow.cpp
@@ -7,12 +7,24 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    // paint text
     QGraphicsScene *scene = new QGraphicsScene;
     scene->setBackgroundBrush(QColor(0, 255, 255, 127));
     QFont font("black", 60);
     scene->addSimpleText("picture", font);
     QGraphicsView *view = new QGraphicsView(scene);
     this->setCentralWidget(view);
+
+    QPen pen;
+    pen.setStyle(Qt::DashDotLine);
+    pen.setWidth(5);
+    pen.setBrush(Qt::red);
+    pen.setCapStyle(Qt::RoundCap);
+    pen.setJoinStyle(Qt::RoundJoin);
+    scene->addLine(20,130,200,30,pen);
+    QGraphicsView *view2 = new QGraphicsView(scene);
+    this->setCentralWidget(view2);
+
 }
 
 MainWindow::~MainWindow()
